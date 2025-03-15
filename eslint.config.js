670b2a5034e860bfb5278e47b528f39a7da0d684
voishu <moonlit644@gmail.com>
2025-03-15 12:28:56 +0100

    chore: http libs (#2)

--- eslint.config.js
+++ eslint.config.js
@@ -14,6 +14,14 @@ export default tseslint.config(
       js.configs.recommended,
       ...tseslint.configs.strictTypeChecked,
       ...tseslint.configs.stylisticTypeChecked,
+      {
+        languageOptions: {
+          parserOptions: {
+            projectService: true,
+            tsconfigRootDir: import.meta.dirname,
+          },
+        },
+      },
       eslintConfigPrettier,
     ],
     files: ["**/*.{ts,tsx}"],
