e2c188ebf9db0a41fdf8ae8041f9ca2b762f0c29
voishu <moonlit644@gmail.com>
2025-03-15 12:26:12 +0100

    chore: http libs

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,19 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import { queryClient } from "./app/query-client.ts";
+import { QueryClientProvider } from "@tanstack/react-query";
 
-createRoot(document.getElementById("root")!).render(
+const root = document.getElementById("root");
+
+if (!root) {
+  throw new Error("Could not find root element");
+}
+
+createRoot(root).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </StrictMode>,
 );
