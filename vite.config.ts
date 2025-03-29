910d34ed1c5836184708c3652dac1aa9d348e83c
voishu <moonlit644@gmail.com>
2025-03-30 00:20:52 +0100

    chore(setup): server address based on env

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,30 @@
-import { defineConfig, UserConfig } from "vite";
+import { defineConfig, loadEnv, UserConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react(), tailwindcss()],
+
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "./src/test/setup.ts",
-  },
-} as UserConfig);
+
+    server: {
+      host: env.VITE_SERVER_HOST || "localhost",
+      port: env.VITE_SERVER_PORT || 3000,
+    },
+
+    test: {
+      globals: true,
+      environment: "jsdom",
+      setupFiles: "./src/test/setup.ts",
+    },
+  } as UserConfig;
+});
